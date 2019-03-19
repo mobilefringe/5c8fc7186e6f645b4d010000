4ee56769c0593ecaced9f2673f90eb8da92d3c64
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-19 13:35:41 -0400

    Anaheim Hills 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -24,7 +24,7 @@
                                 <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
                             </div>
                             <div v-else>
-                                <a :href="banner.url">
+                                <a :href="banner.url" aria-label="banner.name">
                                     <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
                                 </a>
                             </div>
