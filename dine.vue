1da5f133ff78c983601b4c66764969bd5beed9cf
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-19 11:57:26 -0400

    Anaheim Hills 2.0 1.0  /dine.vue

--- dine.vue
+++ dine.vue
@@ -193,6 +193,7 @@
                     
                     subcategories = _.map(subcategories, 'name').sort();
                     subcategories.unshift('All Cuisine');
+                    console.log(subcategories)
                     return subcategories;
                 },
                 filterByCategory() {
