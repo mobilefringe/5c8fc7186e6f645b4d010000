a73c439389c97eab7a9761172b9dec836384ab95
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-03-19 12:06:06 -0400

    Anaheim Hills 2.0 1.0  /events_and_promotions.vue

--- events_and_promotions.vue
+++ events_and_promotions.vue
@@ -113,14 +113,14 @@
             },
             created (){
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Events Banner').images;
-                    if(temp_repo != null) {
-                        this.pageBanner = temp_repo[0];
-                    } else {
-                        this.pageBanner = {
-                            "image_url": "//codecloud.cdn.speedyrails.net/sites/5b5f2c136e6f644fcb5b0100/image/jpeg/1529532304000/insidebanner2.jpg"
-                        }
-                    }
+                    // var temp_repo = this.findRepoByName('Events Banner').images;
+                    // if(temp_repo != null) {
+                    //     this.pageBanner = temp_repo[0];
+                    // } else {
+                    //     this.pageBanner = {
+                    //         "image_url": "//codecloud.cdn.speedyrails.net/sites/5b5f2c136e6f644fcb5b0100/image/jpeg/1529532304000/insidebanner2.jpg"
+                    //     }
+                    // }
 
                     if (_.isEmpty(this.eventList)) {
                         this.toggleEvents = false;
