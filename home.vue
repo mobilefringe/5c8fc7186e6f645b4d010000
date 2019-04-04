e5f2ea31d30cfd7fcb3e7f030047114abe2ccb40
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-04 14:26:20 -0400

    Anaheim Hills 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -20,14 +20,14 @@
                                     </div>
                                 </div>
                             </div>
-                            <!--<div v-else-if="!banner.url">-->
-                            <!--    <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>-->
-                            <!--</div>-->
-                            <!--<div v-else>-->
-                            <!--    <a :href="banner.url" aria-label="banner.name">-->
-                            <!--        <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>-->
-                            <!--    </a>-->
-                            <!--</div>-->
+                            <div v-else-if="!banner.url">
+                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                            </div>
+                            <div v-else>
+                                <a :href="banner.url" aria-label="banner.name">
+                                    <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
+                                </a>
+                            </div>
                         </div>
                     </slick>
                     <div class="next"></div>
