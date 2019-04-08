a20354efb665cedc0a161355c1d5b66de1e3f78c
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-08 17:21:37 -0400

    Anaheim Hills 2.0 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -36,7 +36,7 @@
                             <div class="col-md-12">
                                 <div class="row margin_30">
                                     <div class="col-md-12">
-                                        <router-link to="/promotions">
+                                        <router-link to="/events_and_promotions">
                     		                <div class="animated_btn pull-left">Back to Sales & Promotions</div>    
                     		            </router-link>    
                                     </div>
