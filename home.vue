a5a86782db4fc365a69b81d0a21554295835b920
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-08 16:44:52 -0400

    Anaheim Hills 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -203,17 +203,17 @@
                             }
                         }
                         var temp_image_url = ["//codecloud.cdn.speedyrails.net/sites/5c8fc7186e6f645b4d010000/image/jpeg/1554401780323/ahf_home_1925x470.jpg" ]
-                    var temp_name = ["Lorem Ipsum"]
-                    var temp_desc = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]
-                    var temp_url = ["/stores", "/dine"]
-                    _.forEach(temp_image_url, function (value, key) {
-                        var temp = {};
-                        temp.image_url = temp_image_url[key];
-                        temp.name = temp_name[key];
-                        temp.description = temp_desc[key];
-                        temp.url = temp_url[key];
-                        banners.push(temp);
-                    });
+                        var temp_name = ["Lorem Ipsum"]
+                        var temp_desc = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."]
+                        var temp_url = ["/stores", "/dine"]
+                        _.forEach(temp_image_url, function (value, key) {
+                            var temp = {};
+                            temp.image_url = temp_image_url[key];
+                            temp.name = temp_name[key];
+                            temp.description = temp_desc[key];
+                            temp.url = temp_url[key];
+                            banners.push(temp);
+                        });
                     });
                     
                     banners = _.orderBy(banners, function(o) { return o.position });
