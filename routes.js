65dba277aa44d6f6fc00fbc94a2f96a34b01677d
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-07 23:30:07 -0500

    Anaheim Hills 2.0 1.2  /routes.js

--- routes.js
+++ routes.js
@@ -261,6 +261,20 @@ define([], function () {
                     ]
                 },
                 {
+                    path: '/delivery',
+                    component: view('default'),
+                    children: [
+                        {   
+                            path: '',
+                            component: view('delivery'),
+                            meta: {
+                                breadcrumb: 'Delivery',
+                            },
+                            name: 'delivery'
+                        }
+                    ]
+                },
+                {
                     path: '/map',
                     component: view('default'),
                     children: [
