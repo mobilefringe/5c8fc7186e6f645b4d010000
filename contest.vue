b1cdbc5c4c373a2b0cf2f2458d0d109ce67db383
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2020-04-18 14:03:23 -0400

    Anaheim Hills 2.0 1.2  /contest.vue

--- contest.vue
+++ contest.vue
@@ -73,13 +73,13 @@
         						     <div class="col-xs-12" :class="{'has-error': errors.has('agree_terms')}">
         						        <label class="checkbox">
                                             <input name="agree_terms" type="checkbox" v-model="form_data.terms" required>
-                                                I have read and agree to the <a class="primary" href="/pages/longbeach-rules-and-regulations" target="_blank">Contest Rules & Regulations</a>.
+                                                I have read and agree to the <a class="primary" href="/pages/anaheim-rules-and-regulations" target="_blank">Contest Rules & Regulations</a>.
                                         </label>
         						    </div>
         					    </div>
         					    <div class="form-inline row margin_30">
         						    <div class="col-xs-12">
-        						        <p>For more details about personal privacy, please read our <a class="primary" href="/pages/anaheim-rules-and-regulations" target="_blank">Privacy Policy</a>.</p>
+        						        <p>For more details about personal privacy, please read our <a class="primary" href="/pages/anaheim-privacy-policy" target="_blank">Privacy Policy</a>.</p>
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -134,7 +134,7 @@
                 }); 
                 
                 this.$store.dispatch("getData", "contests").then(response => {
-                    this.currentContest = this.findContestByShowOnSlug('longbeach-contest');
+                    this.currentContest = this.findContestByShowOnSlug('anaheim-contest');
                     if (this.currentContest) {
                         this.dataLoaded = true;
                     } else {
